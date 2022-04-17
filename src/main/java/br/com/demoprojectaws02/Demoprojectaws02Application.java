90726e30bac5fd9154cccaf61bb35d49d8df802b
lelis <lelis685@gmail.com>
2022-04-17 20:49:05 -0300

    first commit

---
+++ src/main/java/br/com/demoprojectaws02/Demoprojectaws02Application.java
@@ -0,0 +1,13 @@
+package br.com.demoprojectaws02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoprojectaws02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoprojectaws02Application.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/demoprojectaws02/enums/EventType.java
@@ -0,0 +1,7 @@
+package br.com.demoprojectaws02.enums;
+
+public enum EventType {
+    PRODUCT_CREATED,
+    PRODUCT_UPDATE,
+    PRODUCT_DELETED
+}
